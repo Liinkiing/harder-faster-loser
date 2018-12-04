8ba93d83b77e98e665999a6bfd3a53e7243391c4
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-04 20:30:40 +0100

    - Added changeState action

--- src/store/GameStore.ts
+++ src/store/GameStore.ts
@@ -1,11 +1,15 @@
-import {observable} from "mobx";
+import {action, observable} from "mobx";
 import {GameState} from "../utils/enums";
 import {GameSettings} from "../utils/interfaces";
 
 class GameStore {
 
   @observable public state: GameState = GameState.Splashscreen
-  @observable public settings: GameSettings = { volume: 1 }
+  @observable public settings: GameSettings = {volume: 1}
+
+  @action public changeState = (newState: GameState) => {
+    this.state = newState
+  }
 
 }
 
