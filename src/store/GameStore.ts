ec6c0a3ec604a09f24296eca8579b345270e48a4
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-04 20:28:51 +0100

    - Added settings in Game store

--- src/store/GameStore.ts
+++ src/store/GameStore.ts
@@ -1,9 +1,11 @@
 import {observable} from "mobx";
 import {GameState} from "../utils/enums";
+import {GameSettings} from "../utils/interfaces";
 
 class GameStore {
 
   @observable public state: GameState = GameState.Splashscreen
+  @observable public settings: GameSettings = { volume: 1 }
 
 }
 
