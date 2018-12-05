ae8fa2b90deb5e4bd4d385b71f30404b1fe79f39
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-05 15:53:02 +0100

    - Added difficulty in GameStore

--- src/store/GameStore.ts
+++ src/store/GameStore.ts
@@ -5,10 +5,15 @@ import {GameSettings} from "../utils/interfaces";
 class GameStore {
 
   @observable public state: GameState = GameState.Splashscreen
+  @observable public difficulity: number = 1
   @observable public settings: GameSettings = {volume: 1}
 
   @action public changeState = (newState: GameState): void => {
     this.state = newState
+  }
+
+  @action public increaseDifficulty = (): void => {
+    this.difficulity++
   }
 
 }
