759931b40ee1ff68114b039064e3600f52f4c768
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-05 01:57:37 +0100

    - Added escape key to exit debug interface

--- src/components/GameDebug.tsx
+++ src/components/GameDebug.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
 import GameDebugStateList from "./ui/debug/GameDebugStateList";
-import {FunctionComponent} from "react";
+import {FunctionComponent, useEffect} from "react";
+import {Key} from "ts-key-enum";
+import gameStore from "../store/GameStore";
 
 const GameDebug: FunctionComponent = () => {
+  const { hideDebug } = gameStore
 
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.key === Key.Escape) {
+        hideDebug()
+      }
+    }
+
+    window.addEventListener('keydown', listener)
+
+    return () => {
+      window.removeEventListener('keydown', listener)
+    }
+  }, [])
   return (
     <div className="game-debug game-ui">
       <h1>Debug interface</h1>
