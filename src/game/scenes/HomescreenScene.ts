46b111b39068cc49994fa8738d22925b28e2eb40
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-06 16:21:04 +0100

    - GameManager now handle switching scene and make sure only one is visible - Added some methods in BaseScene

--- src/game/scenes/HomescreenScene.ts
+++ src/game/scenes/HomescreenScene.ts
@@ -9,4 +9,16 @@ export default class HomescreenScene extends BaseScene {
     });
   }
 
+  public create(): void {
+    const graphics = this.add.graphics()
+    graphics.fillStyle(0xff9933, 1)
+    graphics.fillRect(100,200,600,300)
+    graphics.fillRect(200,100,100,100)
+    this.add.text(220,110,this.scene.key)
+  }
+
+  public update(time: number, delta: number): void {
+  }
+
+
 }
