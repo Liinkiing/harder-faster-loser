95f200c502ce04a1056bb3ac79157028c5fa7b97
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-07 10:48:55 +0100

    - Added GSAP and animations

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,17 @@
+import {TweenLite} from "gsap";
+
 export const wait = (ms: number): Promise<TimerHandler> =>
   new Promise(resolve => setTimeout(resolve, ms))
 
 export const randomRange = (min: number, max: number): number => {
   return Math.random() * (max - min) + min
+}
+
+export const promiseAnimation = (animation: TweenLite | gsap.Animation) => {
+  return new Promise(resolve =>
+    animation
+      .eventCallback('onComplete', async () => {
+        resolve()
+      })
+  )
 }
