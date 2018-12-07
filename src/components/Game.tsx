75e5422ac31d88938232ed82d31cade1e472f0aa
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-07 02:50:33 +0100

    - Added blur when paused

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,16 +8,21 @@ import {observer} from "mobx-react-lite";
 import GameDebugButtonsToolbar from "./ui/debug/GameDebugButtonsToolbar";
 import gameDebugStore from "../store/GameDebugStore";
 import gameManager from "../game/manager/GameManager";
+import gameStore from "../store/GameStore";
 
 const Game: FunctionComponent = () => {
   const { width, height } = useResize()
   const { debug } = gameDebugStore
+  const { paused } = gameStore
 
   if (gameManager.game.canvas) {
+    gameManager.game.canvas.style.transition = 'all 0.15s'
     gameManager.game.canvas.width = width
     gameManager.game.canvas.height = height
     gameManager.game.canvas.style.width = `${width}px`
     gameManager.game.canvas.style.height = `${height}px`
+    gameManager.game.canvas.style.filter = paused ? 'blur(20px)' : null
+    gameManager.game.canvas.style.transform = paused ? 'scale(1.2)' : null
   }
 
   return (
