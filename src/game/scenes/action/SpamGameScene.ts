570c3a360e86f61f367b8118f167e6de1354d1cc
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-07 14:21:54 +0100

    - Moved from static directory

--- src/game/scenes/action/SpamGameScene.ts
+++ src/game/scenes/action/SpamGameScene.ts
@@ -1,29 +1,31 @@
-import {scenesKeys} from "../../../utils/constants";
-import BaseScene from "../BaseScene";
-import Spam from "../../objects/Spam";
-import {List} from "../../../utils/extensions";
-import {randomRange} from "../../../utils/functions";
-import {GameEvents} from "../../../utils/enums";
-import {Emitter} from "../../manager/GameManager";
+import { scenesKeys } from '../../../utils/constants'
+import BaseScene from '../BaseScene'
+import Spam from '../../objects/Spam'
+import { List } from '../../../utils/extensions'
+import { randomRange } from '../../../utils/functions'
+import { GameEvents } from '../../../utils/enums'
+import { Emitter } from '../../manager/GameManager'
 
 export default class SpamGameScene extends BaseScene {
-
   public spams: List<Spam> = new List<Spam>()
 
   constructor() {
     super({
-      key: scenesKeys.SpamGame
-    });
+      key: scenesKeys.SpamGame,
+    })
   }
 
   public preload(): void {
     this.load.pack(
       'preload',
-      '/assets/sprites/spam-game/pack.json',
+      '/static/assets/sprites/spam-game/pack.json',
       'preload'
     )
-    this.load.image('close', '/assets/sprites/spam-game/CLOSE.png')
-    this.load.image('close_active', '/assets/sprites/spam-game/CLOSE_active.png')
+    this.load.image('close', '/static/assets/sprites/spam-game/CLOSE.png')
+    this.load.image(
+      'close_active',
+      '/static/assets/sprites/spam-game/CLOSE_active.png'
+    )
   }
 
   public create() {
@@ -33,7 +35,14 @@ export default class SpamGameScene extends BaseScene {
       this.spams.remove(spam)
     })
     this.input.setGlobalTopOnly(true)
-    const availablesSpam = new List<string>(["sp_1", "sp_2", "sp_3", "sp_4_1", "sp_5_1", "sp_6_1"])
+    const availablesSpam = new List<string>([
+      'sp_1',
+      'sp_2',
+      'sp_3',
+      'sp_4_1',
+      'sp_5_1',
+      'sp_6_1',
+    ])
     this.scene.scene.time.addEvent({
       delay: 1000,
       loop: true,
@@ -43,15 +52,12 @@ export default class SpamGameScene extends BaseScene {
             scene: this,
             x: randomRange(0, window.innerWidth),
             y: randomRange(0, window.innerHeight),
-            spamTexture: availablesSpam.random()
+            spamTexture: availablesSpam.random(),
           })
         )
-      }
+      },
     })
   }
 
-  public update(time: number, delta: number): void {
-  }
-
-
+  public update(time: number, delta: number): void {}
 }
