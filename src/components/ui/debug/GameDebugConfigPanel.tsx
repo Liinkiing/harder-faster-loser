06ff00d3fe8c57c26339fab3325bc61143c9c3d7
Omar Jbara <omar.Jbara2@gmail.com>
2018-12-16 01:50:13 +0100

    - Handle fade toggle

--- src/components/ui/debug/GameDebugConfigPanel.tsx
+++ src/components/ui/debug/GameDebugConfigPanel.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { FunctionComponent, useCallback } from 'react'
+import { FunctionComponent, useCallback, useState } from 'react'
 import DebugContainer from './DebugContainer'
 import { PositionneableProps, TitledProps } from '../../../utils/interfaces'
 import gameStore from '../../../store/GameStore'
@@ -7,16 +7,24 @@ import { observer } from 'mobx-react-lite'
 import { CirclePicker, ColorResult } from 'react-color'
 import Spacer from '../Spacer'
 
-const FadeInput = observer(() => {
+interface FadeInputProps {
+  onFadeChange?: (fade: boolean) => void
+}
+
+const FadeInput = observer((props: FadeInputProps) => {
   const {
     config: { fade },
     changeConfig,
   } = gameStore
+  const { onFadeChange } = props
   const handleFadeChange = useCallback(
     () => {
       changeConfig({
         fade: !fade,
       })
+      if (onFadeChange) {
+        onFadeChange(!fade)
+      }
     },
     [fade]
   )
@@ -34,10 +42,32 @@ const FadeInput = observer(() => {
   )
 })
 
+const colors = [
+  '#f44336',
+  '#e91e63',
+  '#9c27b0',
+  '#673ab7',
+  '#3f51b5',
+  '#2196f3',
+  '#03a9f4',
+  '#00bcd4',
+  '#009688',
+  '#4caf50',
+  '#8bc34a',
+  '#cddc39',
+  '#ffeb3b',
+  '#ffc107',
+  '#ff9800',
+  '#ff5722',
+  '#000000',
+  '#ffffff',
+]
+
 const GameDebugConfigPanel: FunctionComponent<
   PositionneableProps & TitledProps
 > = props => {
   const { changeConfig } = gameStore
+  const [fade, setFade] = useState(true)
   const handleFadeColorChange = useCallback((color: ColorResult) => {
     changeConfig({
       fadeColor: color.hex,
@@ -46,9 +76,18 @@ const GameDebugConfigPanel: FunctionComponent<
 
   return (
     <DebugContainer {...props}>
-      <FadeInput />
-      <Spacer />
-      <CirclePicker onChangeComplete={handleFadeColorChange} />
+      <FadeInput onFadeChange={setFade} />
+      {fade && (
+        <>
+          <Spacer />
+          <p>Fade color</p>
+          <Spacer size="small" />
+          <CirclePicker
+            colors={colors}
+            onChangeComplete={handleFadeColorChange}
+          />
+        </>
+      )}
     </DebugContainer>
   )
 }
