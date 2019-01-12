7f12df48ac29bbf6d0c403ca4604e4a559d73cde
Robin Minervini <robin.minervini@gmail.com>
2019-01-12 21:16:15 +0100

    Add roads and cars dynamically depending of the screen size

--- src/game/scenes/action/TrafficGameScene.ts
+++ src/game/scenes/action/TrafficGameScene.ts
@@ -1,11 +1,20 @@
 import MinigameScene from '../MinigameScene'
 import { scenesKeys } from '../../../utils/constants'
 import gameStore from '../../../store/GameStore'
+import { randomRange } from '../../../utils/functions';
 
 export default class TraficGameScene extends MinigameScene {
   private controls?: Phaser.GameObjects.Container
   private rageBar?: Phaser.GameObjects.Sprite
   private cursorRageBar?: Phaser.GameObjects.Sprite
+  private roads?: Phaser.GameObjects.Sprite[] = []
+  private availableRightCars: string[] = 
+  ["traffic_blue_car_animation", "traffic_cars_01_animation", "traffic_cars_04_animation"]
+  private availableLeftCars: string[] = 
+  ["traffic_cars_02_animation", "traffic_cars_03_animation"]
+  private widthLastCar: number = 0
+  private positionXLastCar: number = 0
+  private positionYLastCar: number = 0
 
   constructor() {
     super({
@@ -21,9 +30,28 @@ export default class TraficGameScene extends MinigameScene {
     super.create()
 
     this.controls = this.createControls()
+    this.createRoad()
+    this.createCars()
   }
 
-  createControls(): Phaser.GameObjects.Container {
+  public onFailure = (): void => {
+    console.log('you failed')
+  }
+
+  public onSuccess = (): void => {
+    console.log('you win')
+  }
+
+  public update = (time: number, delta: number): void => {
+    if (
+      this.cursorRageBar!.x >
+      10 + this.cursorRageBar!.width / gameStore.ratioResolution
+    ) {
+      this.cursorRageBar!.x -= 0.1
+    }
+  }
+
+  private createControls(): Phaser.GameObjects.Container {
     const horn = this.add
       .sprite(0, 0, 'horn_on')
       .setScale(1 / gameStore.ratioResolution)
@@ -55,7 +83,7 @@ export default class TraficGameScene extends MinigameScene {
     return controlsContainer
   }
 
-  public createRageBar(): any {
+  private createRageBar(): any {
     this.rageBar = this.add
       .sprite(0, 0, 'rage_bar')
       .setScale(1 / gameStore.ratioResolution)
@@ -81,20 +109,104 @@ export default class TraficGameScene extends MinigameScene {
     return [this.rageBar, safeRageBarArea, this.cursorRageBar]
   }
 
-  public onFailure = (): void => {
-    console.log('you failed')
+  private createRoad(): void {
+    this.roads![this.roads!.length] = this.add
+      .sprite(0, Number(this.game.config.height), 'road')
+      .setScale(1 / gameStore.ratioResolution)
+      .setOrigin(0, 1)
+
+    let xCounter = 0
+    let yCounter = 0
+    const windowWidth = Number(this.game.config.width)
+    const windowHeight = Number(this.game.config.height)
+    const widthRoadT = this.roads![0].width/gameStore.ratioResolution
+    const heightRoadT = this.roads![0].height/gameStore.ratioResolution
+    const xRepeatCount = Math.ceil(windowWidth/widthRoadT)
+    const yRepeatCount = Math.ceil(windowHeight/heightRoadT)
+
+    const heightRoad = this.roads![0].height
+    const widthRoad = this.roads![0].width
+
+    while (yCounter < yRepeatCount) {
+      xCounter = 0
+
+      while (xCounter < xRepeatCount) {
+        /**
+         * Contenu d'une ligne
+         */
+
+        // Spawn de la route d'une ligne
+        this.roads![this.roads!.length] = this.add
+          .sprite(
+            widthRoad / gameStore.ratioResolution * xCounter,
+            Number(this.game.config.height) - (heightRoad / gameStore.ratioResolution * yCounter),
+            'road')
+          .setScale(1 / gameStore.ratioResolution)
+          .setOrigin(0, 1)
+        xCounter += 1
+      }
+      yCounter += 1
+    }
   }
 
-  public onSuccess = (): void => {
-    console.log('you win')
-  }
+  private createCars(): void {
+    let xCounter = 0
+    let yCounter = 0
 
-  public update = (time: number, delta: number): void => {
-    if (
-      this.cursorRageBar!.x >
-      10 + this.cursorRageBar!.width / gameStore.ratioResolution
-    ) {
-      this.cursorRageBar!.x -= 0.1
+    const windowWidth = Number(this.game.config.width)
+    const windowHeight = Number(this.game.config.height)
+    const widthRoadT = this.roads![0].width/gameStore.ratioResolution
+    const heightRoadT = this.roads![0].height/gameStore.ratioResolution
+    const xRepeatCount = Math.ceil(windowWidth/widthRoadT)
+    const yRepeatCount = Math.ceil(windowHeight/heightRoadT)
+
+    const heightRoad = this.roads![0].height
+
+    let carKey = ""
+
+    let direction = "left"
+
+    while (yCounter < yRepeatCount) {
+      xCounter = 0
+      this.widthLastCar = 0
+      this.positionXLastCar = 0
+
+      if (yCounter%2 === 0) {
+        if (direction === "left") {
+          direction = "right"
+        } else {
+          direction = "left"
+        }
+      }
+
+      
+
+      while (this.positionXLastCar + this.widthLastCar < Number(this.game.config.width)) {
+        console.log('test')
+        if (direction === "right") {
+          carKey = this.availableRightCars[Math.floor(randomRange(0,this.availableRightCars.length - 1))]
+        } else {
+          carKey = this.availableLeftCars[Math.floor(randomRange(0,this.availableLeftCars.length - 1))]
+        }
+
+        const car = this.add.sprite(
+          this.positionXLastCar + this.widthLastCar,
+            Number(this.game.config.height) - (heightRoad / gameStore.ratioResolution * yCounter),
+            carKey
+        )
+        .setScale(1 / gameStore.ratioResolution)
+        .setOrigin(0, 1)
+        .setDepth(1000 - yCounter)
+        car.anims.play(carKey, true)
+    
+        this.widthLastCar = car.width/gameStore.ratioResolution
+        this.positionXLastCar = car.x
+        this.positionYLastCar = car.y
+
+        xCounter += 1
+      }
+      yCounter += 1
     }
   }
 }
+
