f2837c0f27ddabdd9e2fee67f633ec9d52efe028
Omar Jbara <omar.Jbara2@gmail.com>
2019-01-14 14:59:58 +0100

    - Fixes after rebase

--- src/game/scenes/MinigameScene.ts
+++ src/game/scenes/MinigameScene.ts
@@ -3,6 +3,7 @@ import gameManager, { Emitter } from '../manager/GameManager'
 import { GameEvents } from '../../utils/enums'
 import { MinigameGuideline } from '../../utils/interfaces'
 import gameStore from '../../store/GameStore'
+import minigameManager from '../manager/MinigameManager'
 
 export default abstract class MinigameScene extends BaseScene {
   public abstract guideline: MinigameGuideline
@@ -14,14 +15,14 @@ export default abstract class MinigameScene extends BaseScene {
 
   protected onSuccess(): void {
     console.log('you won')
-    gameStore.increaseDifficulty()
+    minigameManager.addCurrentMinigameToPlayedGames()
     gameManager.loadPostMinigame()
   }
 
   protected onFailure(): void {
     console.log('you fail')
-    gameStore.increaseDifficulty()
-    // gameManager.looseLife()
+    minigameManager.addCurrentMinigameToPlayedGames()
+    gameManager.looseLife()
     gameManager.loadPostMinigame()
   }
 
