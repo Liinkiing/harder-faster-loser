99c90e37ca58ce46acd1c9b071c76727191ab50a
Omar Jbara <omar.jbara2@gmail.com>
2019-01-16 00:13:47 +0100

    - Unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import * as ReactDOM from 'react-dom'
 import App from './App'
 import 'gsap/CSSPlugin'
 import './assets/styles/app.scss'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker, { unregister } from './registerServiceWorker'
 import './utils/extensions'
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 // registerServiceWorker()
+unregister()
