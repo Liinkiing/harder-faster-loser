f16f9c1bdaefd23c44b14426bd403f7c8df53210
Omar Jbara <omar.jbara2@gmail.com>
2019-01-15 22:42:28 +0100

    Updates

---
+++ precache-manifest.b19622f0f8ef07c9f10d220ba53a8b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4c45b7c441d6ce0725",
+    "url": "/harder-faster-looser/static/css/main.0603b25c.chunk.css"
+  },
+  {
+    "revision": "9f4c45b7c441d6ce0725",
+    "url": "/harder-faster-looser/static/js/main.9f4c45b7.chunk.js"
+  },
+  {
+    "revision": "b7538eea22c236ce0088",
+    "url": "/harder-faster-looser/static/css/1.0075d791.chunk.css"
+  },
+  {
+    "revision": "b7538eea22c236ce0088",
+    "url": "/harder-faster-looser/static/js/1.b7538eea.chunk.js"
+  },
+  {
+    "revision": "3c53ebe81d6db09cb082",
+    "url": "/harder-faster-looser/static/js/runtime~main.3c53ebe8.js"
+  },
+  {
+    "revision": "2896fca59779fac2cb19a32d3320732f",
+    "url": "/harder-faster-looser/static/media/debug.2896fca5.png"
+  },
+  {
+    "revision": "b0af80553346879d6764699d1635b020",
+    "url": "/harder-faster-looser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/harder-faster-looser/precache-manifest.b8909f658c15ad515e140668d5964ec2.js"
+  "/harder-faster-looser/precache-manifest.b19622f0f8ef07c9f10d220ba53a8b05.js"
 );
 
 workbox.clientsClaim();
