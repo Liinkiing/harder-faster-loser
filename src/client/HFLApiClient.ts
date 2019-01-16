98d5d173757ef5efa2cb7613714e0eccb896a8c3
Omar Jbara <omar.jbara2@gmail.com>
2019-01-16 03:45:54 +0100

    - Added HFLApiClient and Leaderboards store

---
+++ src/client/HFLApiClient.ts
@@ -0,0 +1,81 @@
+import querystring from 'querystring'
+
+const AUTH_HEADER = 'X-AUTH-TOKEN'
+
+type LeaderboardsAllResponse = Array<{
+  score: number
+  username: string
+}>
+
+export type Leaderboards = LeaderboardsAllResponse
+
+type PlayerNewResponse = Array<{
+  score: number
+  username: string
+}>
+
+export type Player = PlayerNewResponse
+
+export default class HFLApiClient {
+  private baseUri: string = process.env.REACT_APP_API_URL || ''
+  private token: string = process.env.REACT_APP_API_TOKEN || ''
+
+  constructor() {
+    if (this.baseUri === '') {
+      throw new Error(
+        'Could not fetch API. Base URI is missing! Please check that your env vars contains a "REACT_APP_API_URL"'
+      )
+    }
+    if (this.token === '') {
+      throw new Error(
+        'Could not get API token. Please check that your env vars contains a "REACT_APP_API_TOKEN"'
+      )
+    }
+  }
+
+  public getAllLeaderboards = async (
+    first: number = 10
+  ): Promise<LeaderboardsAllResponse> => {
+    return (await this.get('/leaderboards/all', { first })).json()
+  }
+
+  public createNewPlayer = async (
+    username: string,
+    score: number
+  ): Promise<PlayerNewResponse> => {
+    return (await this.post('/player/new', { username, score })).json()
+  }
+
+  private prepare = (request: Request): this => {
+    request.headers.set('Accept', 'application/json')
+    request.headers.set('Content-Type', 'application/json')
+    this.auth(request)
+
+    return this
+  }
+
+  private get = async (endpoint: string, query?: object) => {
+    const request = new Request(
+      `${this.baseUri}${endpoint}` +
+        (query ? '?' + querystring.stringify(query) : '')
+    )
+    this.prepare(request)
+
+    return await fetch(request)
+  }
+
+  private post = async (endpoint: string, body: object, query?: object) => {
+    const request = new Request(
+      `${this.baseUri}${endpoint}` +
+        (query ? '?' + querystring.stringify(query) : ''),
+      { method: 'POST', body: JSON.stringify(body) }
+    )
+    this.prepare(request)
+
+    return await fetch(request)
+  }
+
+  private auth = (request: Request) => {
+    request.headers.set(AUTH_HEADER, this.token)
+  }
+}
---
+++ src/store/LeaderboardStore.ts
@@ -0,0 +1,15 @@
+import { action, observable } from 'mobx'
+import HFLApiClient, { Leaderboards } from '../client/HFLApiClient'
+
+class LeaderboardStore {
+  @observable public leaderboards: Leaderboards = []
+
+  constructor(private client: HFLApiClient) {}
+
+  @action public fetchLeaderboards = async () => {
+    this.leaderboards = await this.client.getAllLeaderboards()
+  }
+}
+
+const leaderboardStore = new LeaderboardStore(new HFLApiClient())
+export default leaderboardStore
