b45b2edb544414c3238fad89d065acf409e071c8
Unknown <robin.minervini@gmail.com>
2019-01-27 12:50:13 +0100

    - Added first message on ui

--- src/components/ui/DeathscreenUI.tsx
+++ src/components/ui/DeathscreenUI.tsx
@@ -1,7 +1,23 @@
 import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+
+const Div = styled.div`
+  margin: 80px auto 0 auto;
+  text-align: center;
+  font-size: 30px;
+`
 
 const DeathscreenUI: FunctionComponent = () => {
-  return <div className="deathscreen-ui" />
+  return (
+    <Div className="deathscreen-ui">
+      <p>
+        Toki died
+        <br />
+        of a<br />
+        burn out
+      </p>
+    </Div>
+  )
 }
 
 export default DeathscreenUI
