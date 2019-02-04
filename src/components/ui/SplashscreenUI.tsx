6eb2ff9a826031da3af8f7d94bde572b31781ad1
Omar Jbara <omar.jbara2@gmail.com>
2019-02-04 16:41:49 +0100

    - Added base files for splashscreen

--- src/components/ui/SplashscreenUI.tsx
+++ src/components/ui/SplashscreenUI.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react'
 const SplashscreenUI: FunctionComponent = () => {
   return (
     <div className="splashscreen-ui">
-      <h1>Je suis l'ui du splashscreen</h1>
+      {/*<h1>Je suis l'ui du splashscreen</h1>*/}
     </div>
   )
 }
--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -28,10 +28,9 @@ export default class BootScene extends BaseScene {
 
   public startGame = (): void => {
     gameStore.startGame()
-    gameManager.audio.playBg()
     const initial = gameStore.config.fade
     gameStore.changeConfig({ fade: false })
-    gameManager.loadMinigame('SUBWAY_MINIGAME')
+    gameManager.loadSplashscreen()
     gameStore.changeConfig({ fade: initial })
     this.scene.stop(this.scene.key)
   }
