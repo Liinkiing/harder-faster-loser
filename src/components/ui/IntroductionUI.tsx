d5a97815d6eced9e8c6dab59678c2cec89e80555
Omar Jbara <omar.jbara2@gmail.com>
2019-02-06 02:26:37 +0100

    - Added AutoScroll component and placeholder text

--- src/components/ui/IntroductionUI.tsx
+++ src/components/ui/IntroductionUI.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 import { black } from '../../utils/colors'
+import AutoScroll from './AutoScroll'
+import Spacer from './Spacer'
+import GameButton from './GameButton'
+import gameManager from '../../game/manager/GameManager'
 
 const IntroductionUIInner = styled.div`
   position: fixed;
@@ -11,12 +15,56 @@ const IntroductionUIInner = styled.div`
   width: 100%;
   height: 100%;
   background: ${black};
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 `
 
 const IntroductionUI: FunctionComponent = () => {
+  const { loadHomescreen } = gameManager
   return (
     <IntroductionUIInner>
-      <h1>Je suis l'ui de l'intro</h1>
+      <AutoScroll duration={3000}>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+          accusantium aliquid amet commodi, cupiditate earum eum ipsum
+          laudantium, nisi non omnis perferendis quae quas quasi quos recusandae
+          tenetur ut voluptatum?
+        </div>
+        <Spacer size="large" />
+        <div>
+          Accusamus aspernatur autem corporis, delectus ducimus ea eaque eius
+          esse et expedita in itaque nisi, obcaecati officia possimus quas
+          reprehenderit veniam, voluptate? Adipisci, fuga nihil? Laborum
+          molestias quis vitae voluptatum.
+        </div>
+        <Spacer size="large" />
+        <div>
+          Culpa doloremque dolorum et mollitia repellat! Dignissimos ducimus
+          harum illum ipsum molestias natus, nulla quam rem saepe sequi. Aliquam
+          ducimus ea obcaecati omnis pariatur quibusdam reiciendis! Cum officia
+          reiciendis sit.
+        </div>
+        <Spacer size="large" />
+        <div>
+          Accusamus accusantium ad amet aperiam aspernatur atque aut debitis
+          dolore ex explicabo illo illum inventore maxime natus porro
+          praesentium quibusdam quis recusandae rem totam unde vel voluptate
+          voluptatem voluptates, voluptatibus?
+        </div>
+        <Spacer size="large" />
+        <div>
+          Consequatur ea, vel. A ab architecto dolorem doloremque ea esse
+          eveniet explicabo inventore iste magni minima nesciunt, nihil nostrum
+          officiis pariatur perspiciatis quasi qui quibusdam quidem, quo sunt
+          tempora voluptatibus.
+        </div>
+        <Spacer size="large" />
+        <GameButton size="small" onClick={loadHomescreen}>
+          Wake toki
+        </GameButton>
+        <Spacer size="large" />
+      </AutoScroll>
     </IntroductionUIInner>
   )
 }
