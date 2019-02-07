337636d8d017b88101c166257da185b97495d5c9
Omar Jbara <omar.jbara2@gmail.com>
2019-02-07 16:54:50 +0100

    - Added Action indicator in Homescreen but disabled it while we wait for new assets

--- src/game/scenes/HomescreenScene.ts
+++ src/game/scenes/HomescreenScene.ts
@@ -15,6 +15,7 @@ export default class HomescreenScene extends BaseScene {
     super.create()
     gameManager.audio.playBg()
     gameManager.changeBackgroundColor(darkBlue)
+    // this.createActionIndicator()
     this.add
       .sprite(
         window.innerWidth / 2,
@@ -27,4 +28,13 @@ export default class HomescreenScene extends BaseScene {
   }
 
   public update(time: number, delta: number): void {}
+
+  private createActionIndicator = () => {
+    this.add
+      .sprite(window.innerWidth / 2, window.innerHeight / 2 - 220, 'shake')
+      .setOrigin(0.5, 0.5)
+      .setScale(3)
+      .setDepth(9999)
+      .play('shake_animation')
+  }
 }
