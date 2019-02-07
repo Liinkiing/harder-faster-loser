6c5e9cb695d0bcf1f5f37aee63993887c735f7dd
Unknown <robin.minervini@gmail.com>
2019-02-07 15:53:10 +0100

    - Added CountUp component to death screen to animate the percent value of the data - Added real datas

--- src/components/ui/DeathscreenUI.tsx
+++ src/components/ui/DeathscreenUI.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import gameManager, { Emitter } from '../../game/manager/GameManager'
 import { GameEvents } from '../../utils/enums'
 import GameButton from './GameButton'
+import CountUp from 'react-countup'
 
 const Div = styled.div`
   position: fixed;
@@ -25,6 +26,10 @@ const Div = styled.div`
   }
 `
 
+const PercentData = styled.div`
+  display: flex;
+`
+
 const DeathscreenUI: FunctionComponent = () => {
   const { loadLeaderboards } = gameManager
   const [showButton, setShowButton] = useState(false)
@@ -33,9 +38,11 @@ const DeathscreenUI: FunctionComponent = () => {
       Toki died <br /> of a<br /> burn out{' '}
     </p>
   )
+  const [percent, setPercent] = useState(0)
 
   useEffect(() => {
     Emitter.on(GameEvents.DeathscreenFirstSceneDestroyed, args => {
+      setPercent(args.percent)
       setMessage(<p>{args.text}</p>)
       setShowButton(true)
     })
@@ -43,6 +50,13 @@ const DeathscreenUI: FunctionComponent = () => {
 
   return (
     <Div className="deathscreen-ui">
+      {showButton && (
+        <PercentData>
+          <CountUp start={0} end={percent} />
+          <span>%</span>
+        </PercentData>
+      )}
+
       {message}
       {showButton && (
         <GameButton onClick={loadLeaderboards} size="small">
