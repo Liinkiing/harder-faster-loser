27f48b43df39cc7bd23dfdaa7827f225053e6622
Omar Jbara <omar.jbara2@gmail.com>
2019-02-11 19:26:27 +0100

    - Added zzz sleeping animation

--- src/game/objects/homescreen/TokiDreams.ts
+++ src/game/objects/homescreen/TokiDreams.ts
@@ -2,6 +2,9 @@ import { SpriteConstructor } from '../../../utils/interfaces'
 import { Omit } from '../../../utils/types'
 import gameStore from '../../../store/GameStore'
 
+const Y_OFFSET = 300
+const X_OFFSET = -120
+
 export default class TokiDreams extends Phaser.GameObjects.Sprite {
   public static positionsOffset = [
     window.innerWidth / 2,
@@ -35,6 +38,8 @@ export default class TokiDreams extends Phaser.GameObjects.Sprite {
     window.innerWidth / 2 + 10 + 20,
   ]
 
+  private zzz: Phaser.GameObjects.Sprite
+
   constructor(params: Omit<SpriteConstructor, 'texture' | 'frame'>) {
     super(params.scene, params.x, params.y, '')
     const { getAnimationName } = this
@@ -45,7 +50,35 @@ export default class TokiDreams extends Phaser.GameObjects.Sprite {
         this.play(getAnimationName())
       })
 
+    this.zzz = new Phaser.GameObjects.Sprite(
+      params.scene,
+      this.x + X_OFFSET,
+      this.y + Y_OFFSET,
+      'intro_zzz'
+    )
+      .setScale(12 / gameStore.ratioResolution)
+      .play('intro_zzz_animation')
+
     params.scene.add.existing(this)
+    params.scene.add.existing(this.zzz)
+  }
+
+  public setZzzPosition = (
+    x?: number,
+    y?: number,
+    z?: number,
+    w?: number
+  ): void => {
+    const [xPos, yPos] = [
+      x ? x + X_OFFSET : X_OFFSET,
+      y ? y + Y_OFFSET : Y_OFFSET,
+    ]
+    this.zzz.setPosition(xPos, yPos, z, w)
+  }
+
+  public destroy(fromScene?: boolean): void {
+    super.destroy(fromScene)
+    this.zzz.destroy(fromScene)
   }
 
   private getAnimationName = (): string => {
